ec1e428cffdfd2af74d0e009241ecdf10f12b3f3
The Ambassador of Funk <acobster@gmail.com>
2014-12-16 13:28:17 -0800

    simplify index.php

--- index.php
+++ index.php
@@ -5,22 +5,14 @@ error_reporting( E_ERROR | E_PARSE );
 require './autoload.php';
 if( file_exists('./tweb_config.php') ) require './tweb_config.php';
 
-try {
-  if( !empty($_POST) ) {
-    // TODO implement or import OrderProvider
-    $orderProvider = array();
+if( !empty($_POST) ) {
+  // TODO implement or import OrderProvider
+  $orderProvider = array();
 
-    $thub = new THub\THubService( $orderProvider );
-    echo $thub->parseRequest( $_POST['XML'] );
-  } else {
-    header('Allow: POST');
-    http_response_code(405);
-    echo "Please use the HTTP POST method!";
-  }
-} catch( THub\AuthException $e ) {
-  http_response_code(401);
-  echo "401 Unauthorized";
-} catch( THub\BadRequestException $e ) {
-  http_response_code(400);
-  echo "400 Bad Request: " . $e->getMessage();
+  $thub = new THub\THubService( $orderProvider );
+  echo $thub->parseRequest( $_POST['XML'] );
+} else {
+  header('Allow: POST');
+  http_response_code(405);
+  echo "Please use the HTTP POST method!";
 }
