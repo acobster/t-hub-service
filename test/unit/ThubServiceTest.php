50fed5a8ec3271429ac6ef91e580111e2771296c
The Ambassador of Funk <acobster@gmail.com>
2014-12-16 16:38:36 -0800

    Spec billing and shipping details

--- test/unit/ThubServiceTest.php
+++ test/unit/ThubServiceTest.php
@@ -3,14 +3,132 @@
 require 'lib/THub/ThubService.php';
 
 class THubServiceTest extends PHPUnit_Framework_TestCase {
-  public static $order1 = array(
-    'id' => '1',
-    'transaction_type' => 'Sale',
-  );
+  public static $orders = array(
+    array(
+      'id'                => '1',
+      'ref_id'            => '123456789',
+      'transaction_type'  => 'Sale',
+      'date'              => '2014-16-12',
+      'time'              => '11:40:21',
+      'time_zone'         => 'PST',
+      'updated_on'        => '2014-16-12 22:40:21',
+      'bill'              => array(
+        'pay_method'        => 'CreditCard',
+        'pay_status'        => 'Pending',
+        'pay_date'          => '2014-16-12',
+        'first_name'        => 'Bob',
+        'last_name'         => 'Barker',
+        'middle_name'       => 'Billy',
+        'company_name'      => 'ACME',
+        'address1'          => '123 Fake St.',
+        'address2'          => 'Ste. 500',
+        'city'              => 'Tacoma',
+        'state'             => 'WA',
+        'zip'               => '12345',
+        'country'           => 'USA',
+        'email'             => 'me@email.com',
+        'phone'             => '123-456-2345',
+        'po_number'         => '1234',
+        'credit_card'             => array(
+          'credit_card_type'      => 'VISA',
+          'credit_card_charge'    => '100.00',
+          'expiration_date'       => '12/2018',
+          'credit_card_number'    => 'XXXXXXXXXXXX1234',
+          'cvv2'                  => '321',
+          'transaction_id'        => '456789',
+          'settlement_batch_id'   => '34578',
+          'reconciliation_data'   => '...Some data...',
+        ),
+      ),
+      'ship' => array(
+        'ship_status'       => 'New',
+        'ship_carrier_name' => 'FedEx',
+        'ship_method'       => 'Ground',
+        'first_name'        => 'Bob',
+        'last_name'         => 'Barker',
+        'middle_name'       => 'Billy',
+        'company_name'      => 'ACME',
+        'address1'          => '123 Fake St.',
+        'address2'          => 'Ste. 500',
+        'city'              => 'Tacoma',
+        'state'             => 'WA',
+        'zip'               => '12345',
+        'country'           => 'USA',
+        'email'             => 'me@email.com',
+        'phone'             => '123-456-2345',
+      ),
+      'items' => array(
+        array(
 
-  public static $order2 = array(
-    'id' => '2',
-    'transaction_type' => 'Sale',
+        ),
+        array(
+
+        ),
+      ),
+      'charges' => array(
+        'coupons' => array(
+        ),
+      ),
+    ),
+    array(
+      'id'                => '2',
+      'ref_id'            => '234567890',
+      'transaction_type'  => 'Sale',
+      'date'              => '2014-16-12',
+      'time'              => '11:45:21',
+      'time_zone'         => 'PST',
+      'updated_on'        => '2014-16-12 22:45:21',
+      'bill'              => array(
+        'pay_method'        => 'CreditCard',
+        'pay_status'        => 'Pending',
+        'pay_date'          => '2014-16-12',
+        'first_name'        => 'Bob',
+        'last_name'         => 'Barker',
+        'middle_name'       => 'Billy',
+        'company_name'      => 'ACME',
+        'address1'          => '123 Fake St.',
+        'address2'          => 'Ste. 500',
+        'city'              => 'Tacoma',
+        'state'             => 'WA',
+        'zip'               => '12345',
+        'country'           => 'USA',
+        'email'             => 'me@email.com',
+        'phone'             => '123-456-2345',
+        'po_number'         => '1234',
+      ),
+      'ship' => array(
+        'ship_status'       => 'Shipped',
+        'ship_date'         => '2014-12-01',
+        'ship_tracking'     => 'TRACKING-NO-234',
+        'ship_cost'         => '4.20',
+        'ship_carrier_name' => 'USPS',
+        'ship_method'       => 'Ground',
+        'first_name'        => 'Bob',
+        'last_name'         => 'Barker',
+        'middle_name'       => 'Billy',
+        'company_name'      => 'ACME',
+        'address1'          => '123 Fake St.',
+        'address2'          => 'Ste. 500',
+        'city'              => 'Tacoma',
+        'state'             => 'WA',
+        'zip'               => '12345',
+        'country'           => 'USA',
+        'email'             => 'me@email.com',
+        'phone'             => '123-456-2345',
+      ),
+      'items' => array(
+        array(
+
+        ),
+        array(
+
+        ),
+      ),
+      'charges' => array(
+        'coupons' => array(
+        ),
+      ),
+    ),
   );
 
   public function setUp() {
@@ -79,7 +197,7 @@ class THubServiceTest extends PHPUnit_Framework_TestCase {
 
   public function testGetOrdersXmlWithArray() {
     $this->mockProvider->method('getNewOrders')
-      ->willReturn( array( self::$order1, self::$order2 ));
+      ->willReturn( self::$orders );
 
     $parsed = $this->getParsedResponse( self::GET_ORDERS_REQUEST_XML );
     $this->assertEquals( 'GetOrders', $parsed->Envelope->Command );
@@ -90,8 +208,8 @@ class THubServiceTest extends PHPUnit_Framework_TestCase {
 
     $orders = $parsed->Orders->Order;
 
-    $this->assertOrder( self::$order1, $orders[0] );
-    $this->assertOrder( self::$order2, $orders[1] );
+    $this->assertOrder( self::$orders[0], $orders[0] );
+    $this->assertOrder( self::$orders[1], $orders[1] );
   }
 
   public function testDecodeElement() {
@@ -112,8 +230,92 @@ class THubServiceTest extends PHPUnit_Framework_TestCase {
   }
 
   protected function assertOrder( $expected, $actual ) {
-    $this->assertEquals( $expected['id'], $actual->OrderID );
+    $this->assertEquals( $expected['id'],               $actual->OrderID );
+    $this->assertEquals( $expected['ref_id'],           $actual->ProviderOrderRef );
     $this->assertEquals( $expected['transaction_type'], $actual->TransactionType );
+    $this->assertEquals( $expected['date'],             $actual->Date );
+    $this->assertEquals( $expected['time'],             $actual->Time );
+    $this->assertEquals( $expected['time_zone'],        $actual->TimeZone );
+    $this->assertEquals( $expected['updated_on'],       $actual->UpdatedOn );
+
+    $this->assertOrderBill( $expected['bill'],          $actual->Bill );
+    $this->assertOrderShip( $expected['ship'],          $actual->Ship );
+    // $this->assertOrderItems( $expected['items'],        $actual->OrderItems );
+    // $this->assertOrderCharges( $expected['charges'],    $actual->OrderCharges );
+  }
+
+  protected function assertOrderBill( $expected, $actual ) {
+    $this->assertEquals( $expected['pay_method'],   $actual->PayMethod );
+    $this->assertEquals( $expected['pay_status'],   $actual->PayStatus );
+    $this->assertEquals( $expected['first_name'],   $actual->FirstName );
+    $this->assertEquals( $expected['last_name'],    $actual->LastName );
+    $this->assertEquals( $expected['address1'],     $actual->Address1 );
+    $this->assertEquals( $expected['address2'],     $actual->Address2 );
+    $this->assertEquals( $expected['city' ],        $actual->City );
+    $this->assertEquals( $expected['state'],        $actual->State );
+    $this->assertEquals( $expected['zip'],          $actual->Zip );
+    $this->assertEquals( $expected['country'],      $actual->Country );
+    $this->assertEquals( $expected['email'],        $actual->Email );
+    $this->assertEquals( $expected['phone'],        $actual->Phone );
+
+    $this->assertEqualsIfPresent( $expected['pay_date'],     $actual->PayDate );
+    $this->assertEqualsIfPresent( $expected['middle_name'], $actual->MiddleName );
+    $this->assertEqualsIfPresent( $expected['company_name'], $actual->CompanyName );
+    $this->assertEqualsIfPresent( $expected['address2'],     $actual->Address2 );
+    $this->assertEqualsIfPresent( $expected['po_number'], $actual->PONumber );
+    $this->assertEqualsIfPresent( $expected['payment_amount'], $actual->PaymentAmount );
+
+    if( $expected['credit_card'] )
+      $this->assertOrderBillCard( $expected['credit_card'], $actual->CreditCard );
+  }
+
+  protected function assertOrderBillCard( $expected, $actual ) {
+    $this->assertEquals( $expected['credit_card_type'],     $actual->CreditCardType );
+    $this->assertEquals( $expected['credit_card_charge'],   $actual->CreditCardCharge );
+    $this->assertEquals( $expected['expiration_date'],      $actual->ExpirationDate );
+    $this->assertEquals( $expected['credit_card_number'],   $actual->CreditCardNumber );
+
+    $this->assertEqualsIfPresent( $expected['cvv2'], $actual->CVV2 );
+    $this->assertEqualsIfPresent( $expected['auth_details'], $actual->AuthDetails );
+    $this->assertEqualsIfPresent( $expected['transaction_id'], $actual->TransactionID );
+    $this->assertEqualsIfPresent( $expected['settlement_batch_id'], $actual->SettlementBatchID );
+    $this->assertEqualsIfPresent( $expected['reconciliation_data'], $actual->ReconciliationData );
+  }
+
+  protected function assertOrderShip( $expected, $actual ) {
+    $this->assertEquals( $expected['ship_carrier_name'],    $actual->ShipCarrierName );
+    $this->assertEquals( $expected['ship_method'],          $actual->ShipMethod );
+    $this->assertEquals( $expected['first_name'],           $actual->FirstName );
+    $this->assertEquals( $expected['last_name'],            $actual->LastName );
+    $this->assertEquals( $expected['address1'],             $actual->Address1 );
+    $this->assertEquals( $expected['city'],                 $actual->City );
+    $this->assertEquals( $expected['state'],                $actual->State );
+    $this->assertEquals( $expected['zip'],                  $actual->Zip );
+    $this->assertEquals( $expected['country'],              $actual->Country );
+    $this->assertEquals( $expected['email'],                $actual->Email );
+    $this->assertEquals( $expected['phone'],                $actual->Phone );
+
+    $this->assertEqualsIfPresent( $expected['ship_status'],     $actual->ShipStatus );
+    $this->assertEqualsIfPresent( $expected['ship_date'],       $actual->ShipDate );
+    $this->assertEqualsIfPresent( $expected['ship_tracking'],   $actual->ShipTracking );
+    $this->assertEqualsIfPresent( $expected['ship_cost'],       $actual->ShipCost );
+    $this->assertEqualsIfPresent( $expected['middle_name'],     $actual->MiddleName );
+    $this->assertEqualsIfPresent( $expected['company_name'],    $actual->CompanyName );
+    $this->assertEqualsIfPresent( $expected['address2'],        $actual->Address2 );
+  }
+
+  protected function assertOrderItems( $expected, $actual ) {
+
+  }
+
+  protected function assertOrderCharges( $expected, $actual ) {
+
+  }
+
+  protected function assertEqualsIfPresent( $expected, $actual ) {
+    if( !empty($expected) ) {
+      $this->assertEquals( $expected, $actual );
+    }
   }
 
   protected function getParsedResponse( $request ) {
--- views/thub/response.php
+++ views/thub/response.php
@@ -13,9 +13,116 @@
           <OrderID><?= $order['id'] ?></OrderID>
           <ProviderOrderRef><?= $order['ref_id'] ?></ProviderOrderRef>
           <TransactionType><?= $order['transaction_type'] ?></TransactionType>
+
           <?php if( $order['transaction_type'] == self::TYPE_RETURN ) : ?>
-          <OrigOrderID><?= $order['orig_id'] ?></OrigOrderID>
+            <OrigOrderID><?= $order['orig_id'] ?></OrigOrderID>
           <?php endif; ?>
+
+          <Date><?= $order['date'] ?></Date>
+          <Time><?= $order['time'] ?></Time>
+          <TimeZone><?= $order['time_zone'] ?></TimeZone>
+          <UpdatedOn><?= $order['updated_on'] ?></UpdatedOn>
+
+          <Bill>
+            <?php $bill = $order['bill']; ?>
+            <PayMethod><?= $bill['pay_method'] ?></PayMethod>
+            <PayStatus><?= $bill['pay_status'] ?></PayStatus>
+            <PayDate><?= $bill['pay_date'] ?></PayDate>
+            <FirstName><?= $bill['first_name'] ?></FirstName>
+            <LastName><?= $bill['last_name'] ?></LastName>
+            <MiddleName><?= $bill['middle_name'] ?></MiddleName>
+            <CompanyName><?= $bill['company_name'] ?></CompanyName>
+            <Address1><?= $bill['address1'] ?></Address1>
+            <Address2><?= $bill['address2'] ?></Address2>
+            <City><?= $bill['city' ] ?></City>
+            <State><?= $bill['state'] ?></State>
+            <Zip><?= $bill['zip'] ?></Zip>
+            <Country><?= $bill['country'] ?></Country>
+            <Email><?= $bill['email'] ?></Email>
+            <Phone><?= $bill['phone'] ?></Phone>
+            <PONumber><?= $bill['po_number'] ?></PONumber>
+
+            <?php if( $card = $bill['credit_card'] ) : ?>
+              <CreditCard>
+                <CreditCardType><?= $card['credit_card_type'] ?></CreditCardType>
+                <CreditCardCharge><?= $card['credit_card_charge'] ?></CreditCardCharge>
+                <ExpirationDate><?= $card['expiration_date'] ?></ExpirationDate>
+                <CreditCardNumber><?= $card['credit_card_number'] ?></CreditCardNumber>
+
+                <?php if( $card['cvv2'] ) : ?>
+                  <CVV2><?= $card['cvv2'] ?></CVV2>
+                <?php endif; ?>
+
+                <?php if( $card['transaction_id'] ) : ?>
+                  <TransactionID><?= $card['transaction_id'] ?></TransactionID>
+                <?php endif; ?>
+
+                <?php if( $card['settlement_batch_id'] ) : ?>
+                  <SettlementBatchID><?= $card['settlement_batch_id'] ?></SettlementBatchID>
+                <?php endif; ?>
+
+                <?php if( $card['auth_details'] ) : ?>
+                  <AuthDetails><?= $card['auth_details'] ?></AuthDetails>
+                <?php endif; ?>
+
+                <?php if( $card['reconciliation_data'] ) : ?>
+                  <ReconciliationData><?= $card['reconciliation_data'] ?></ReconciliationData>
+                <?php endif; ?>
+
+              </CreditCard>
+            <?php endif; ?>
+          </Bill>
+
+          <Ship>
+            <?php $ship = $order['ship']; ?>
+            <ShipCarrierName><?= $ship['ship_carrier_name'] ?></ShipCarrierName>
+            <ShipMethod><?= $ship['ship_method'] ?></ShipMethod>
+            <FirstName><?= $ship['first_name'] ?></FirstName>
+            <LastName><?= $ship['last_name'] ?></LastName>
+            <Address1><?= $ship['address1'] ?></Address1>
+            <City><?= $ship['city'] ?></City>
+            <State><?= $ship['state'] ?></State>
+            <Zip><?= $ship['zip'] ?></Zip>
+            <Country><?= $ship['country'] ?></Country>
+            <Email><?= $ship['email'] ?></Email>
+            <Phone><?= $ship['phone'] ?></Phone>
+
+            <?php if( $ship['ship_status'] ) : ?>
+              <ShipStatus><?= $ship['ship_status'] ?></ShipStatus>
+            <?php endif; ?>
+
+            <?php if( $ship['ship_date'] ) : ?>
+              <ShipDate><?= $ship['ship_date'] ?></ShipDate>
+            <?php endif; ?>
+
+            <?php if( $ship['ship_tracking'] ) : ?>
+              <ShipTracking><?= $ship['ship_tracking'] ?></ShipTracking>
+            <?php endif; ?>
+
+            <?php if( $ship['ship_cost'] ) : ?>
+              <ShipCost><?= $ship['ship_cost'] ?></ShipCost>
+            <?php endif; ?>
+
+            <?php if( $ship['middle_name'] ) : ?>
+              <MiddleName><?= $ship['middle_name'] ?></MiddleName>
+            <?php endif; ?>
+
+            <?php if( $ship['company_name'] ) : ?>
+              <CompanyName><?= $ship['company_name'] ?></CompanyName>
+            <?php endif; ?>
+
+            <?php if( $ship['address2'] ) : ?>
+              <Address2><?= $ship['address2'] ?></Address2>
+            <?php endif; ?>
+          </Ship>
+
+          <Items>
+
+          </Items>
+
+          <Charges>
+
+          </Charges>
         </Order>
       <?php endforeach; ?>
     </Orders>
