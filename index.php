78ecde6c3db1466c9a753f93c25c9ee838a8c1ce
The Ambassador of Funk <acobster@gmail.com>
2017-10-09 11:13:26 -0700

    parse entire raw POST body

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ error_reporting( E_ERROR | E_PARSE | E_RECOVERABLE_ERROR );
 require './autoload.php';
 if( file_exists('./thub_config.php') ) require_once './thub_config.php';
 
-if( $_POST ) {
+if( $xml = file_get_contents('php://input') ) {
   $orderProvider = new Data\OrderModel();
   $thub = new THub\THubService( $orderProvider );
-  echo $thub->parseRequest( $_POST['XML'] );
+  echo $thub->parseRequest( $xml );
 } else {
   header('Allow: POST');
   header('HTTP/1.0 405 Method Not Allowed');
