aab7ca440b016f7fdf38cad709c5875c41528fb6
The Ambassador of Funk <acobster@gmail.com>
2018-03-12 19:08:05 -0700

    Parse POST request param

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting( E_ERROR | E_PARSE | E_RECOVERABLE_ERROR );
 require './autoload.php';
 if( file_exists('./thub_config.php') ) require_once './thub_config.php';
 
-if( $xml = file_get_contents('php://input') ) {
+if( $xml = $_POST['request'] ) {
   $orderProvider = new Data\OrderModel();
   $thub = new THub\THubService( $orderProvider );
   echo $thub->parseRequest( $xml );
